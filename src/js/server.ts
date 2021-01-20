cf93aac061e17b8e465449c2a5972c54a5cf2d99
mooner1022 <moonmk10221@gmail.com>
2021-01-21 00:14:12 +0900

    Fix conflict and directory

--- src/js/server.ts
+++ src/js/server.ts
@@ -3,13 +3,12 @@ const path = require('path');
 const app = express();
 
 let srcPath = `${__dirname}/../`;
-let htmlPath = path.join(srcPath, 'html');
 
 app.use(express.static(srcPath));
 //app.use(express.static(htmlPath));
 
 app.get('/', (req : express.Request , res : express.Response) => {
-    res.sendFile(path.join(htmlPath, 'DAM.html'));
+    res.sendFile(path.join(srcPath, 'DAM.html'));
 });
 app.listen(80, () => {
     console.log('Express App on port 80!');

e8138ae6e29b7c99aa6103d90f03e71b70cedc80
mooner1022 <moonmk10221@gmail.com>
2021-02-24 17:51:45 +0900

    Update profile page

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,7 +1,16 @@
 $(()=>{
     let accessToken = Cookies.get(ACCESS_TOKEN_KEY);
 
+    /*
+     * 유저 정보 쿠키로부터 로드 후 적용
+     */
     $("#name").text(parseJwt(accessToken).name)
     $("#userid").text(parseJwt(accessToken).id)
+    
+    /*
+     * 비밀번호 변경 버튼 로직
+     */
+    $("#button-change-pw").on('click',()=> {
 
+    })
 })

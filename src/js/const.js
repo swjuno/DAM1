3fa3af4b94ee108b10fba020b7154f5ac582fef4
mooner <moonmk10221@gmail.com>
2021-03-10 01:41:26 +0900

    Update const.js

--- src/js/const.js
+++ src/js/const.js
@@ -1,4 +1,4 @@
 const isDeveloping = false;
 
-const SERVER_ADDR = isDeveloping?"http://localhost:8080":"http://182.209.63.121:8080";
+const SERVER_ADDR = isDeveloping?"http://localhost:8080":"https://server.mooner.dev:8443";
 const ACCESS_TOKEN_KEY = "access_token";
